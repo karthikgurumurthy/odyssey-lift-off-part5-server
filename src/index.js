855e1062d2124f42cf28e0e2b6432d89ad2bd485
Gurumurthy, Karthik <a514035@fmr.com>
2022-08-09 13:45:43 -0400

    liftoff5 port config completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
+const { ApolloServer } = require('apollo-server')
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+const TrackAPI = require('./datasources/track-api')
+const {
+  ApolloServerPluginLandingPageLocalDefault,
+} = require('apollo-server-core')
+const { bootstrap: bootstrapGlobalAgent } = require('global-agent')
+
+bootstrapGlobalAgent()
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -10,16 +16,16 @@ async function startApolloServer(typeDefs, resolvers) {
     dataSources: () => {
       return {
         trackAPI: new TrackAPI(),
-      };
+      }
     },
-  });
+  })
 
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 })
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
       📭  Query at ${url}
-    `);
+    `)
 }
 
-startApolloServer(typeDefs, resolvers);
+startApolloServer(typeDefs, resolvers)
